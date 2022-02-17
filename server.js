14a73d114fcfab2a591d901967b12aa8a6ff29ad
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-16 21:36:08 -0800

    server js file complete. getting routes error

--- server.js
+++ server.js
@@ -2,8 +2,44 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require 
+const routes = require('./controllers') ;
+const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connections');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: "super duper secret",
+    cookie: {
+        maxAge: 86400 
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(express.static(patj.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force : false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`))
+})
 
 
 console.log("get your read on");
