457b63e94ae7a57a2e9d5febe12c46c5ab30088a
Emma Mondul <92192222+emondul@users.noreply.github.com>
2022-02-16 19:19:57 -0800

    adding models (#7)

---
+++ models/Book.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    checked_in: {
+      type: DataTypes.BOOLEAN,
+    },
+    new_arrival: {
+        type: DataTypes.BOOLEAN,
+      },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'dish',
+  }
+);
+
+module.exports = Book;
