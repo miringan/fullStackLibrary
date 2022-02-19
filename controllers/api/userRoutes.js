f6d3fbc8a85b1592fe5a712629c57d32b4f0e330
Mikayla Bruce <89227020+MikiWolfe@users.noreply.github.com>
2022-02-19 11:52:49 -0800

    Route tests (#14)
    * wrong branch
    
    * routes have been tested

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Get
+router.get('/', async (req, res) => {
+    try {
+const dbUserData = await User.findAll({
+    attributes: ["user_name"],
+    exclude : ["password"],
+})
+res.status(200).json(dbUserData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // POST route to create a new user.
 router.post('/', async (req, res) => {
     try {
