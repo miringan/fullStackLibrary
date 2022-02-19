d6a7d4ab9459c808433917b31c55ec4d95715917
Luke <lukeconrad.stew@gmail.com>
2022-02-19 10:28:22 -0800

    routes files

--- server.js
+++ server.js
@@ -1,1 +1,52 @@
-console.log("get your read on")
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers') ;
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connections');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: "super duper secret",
+    cookie: {
+        maxAge: 86400 
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(express.static(patj.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force : false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`))
+})
+
+
+console.log("get your read on");
+
+
+
+
+
+
+
