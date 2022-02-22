0419b08ca1d45b762588cecf07d7ea3325306c6e
Michael Iringan <miringan@uw.edu>
2022-02-21 17:20:37 -0800

    re-pull from github repo.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,62 +3,23 @@ const { User, Book } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET route for home page
-router.get('/', async (req, res) => {
-    try {
-        // Pass serialized data and session flag into template
-        res.render('homepage', { 
-            projects, 
-            logged_in: req.session.logged_in 
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         // Pass serialized data and session flag into template
+//         res.render('homepage', { 
+//             projects, 
+//             logged_in: req.session.logged_in 
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
-// Still WIP
-// GET route for library
-router.get('/library', async (req,res) => {
-    try{ 
-        const libraryData = await Project.findAll({
-            include: [
-                {
-                    model: Book,
-                        // Need to tie in search result from homepage search bar into attributes
-                        attributes: ['title'],
-                },
-                ],
-        });
-        // Serialize data so the template can read it
-        const libraries = libraryData.map((library) => library.get({ plain: true }));
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// PUT route to checkout a book from the library
-router.put('/library/:title', (req, res) => {
-    // Calls the update method on the Book model
-    Book.update(
-      {
-        // All the fields you can update and the data attached to the request body.
-        checked_in: false
-      },
-      {
-        // Gets the books based on the title given in the request parameters
-        where: {
-            // Need to link search result to req 
-            title: req.params.title,
-        },
-      }
-    )
-      .then((updatedBook) => {
-        // Sends the updated book as a json response
-        res.json(updatedBook);
-      })
-      .catch((err) => res.json(err));
+router.get("/", async (req, res) => {
+    const postData = await Post.findAll();
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
   });
-
-// POST route to add a book to the library
-
-
-// POST route to create a user
