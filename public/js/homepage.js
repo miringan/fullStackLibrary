56add42e6946fb5f29b7601d3d16d7b22ebd27e5
Lucas Stewart <lukeconrad.stew@gmail.com>
2022-02-26 15:09:16 -0800

    Renderbook (#29)
    * test update works
    
    * testing testing 1 2 1 2
    
    * trying to render book data to the page

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -12,18 +12,36 @@ const searchBookTitle = async (event) => {
         "Content-Type": "application/json",
       },
     });
+    console.log(response);
 
     if (response.ok) {
       localStorage.setItem("bookSearch", JSON.stringify(response));
-      document.location.replace("/bookInformation");
+      document.location.replace("/bookInfo");
     } else {
       alert("Failure");
+      document.location.replace("/homepage");
     }
-    // const bookResponse = await response.json();
-      // console.log(bookResponse);
-  
+    const bookResponse = await response.json();
+    console.log(bookResponse);
+
   }
 };
+
+// function getDataApi() {
+//   let searchResults = document.getElementById("searchResults").innerHTML = 
+// };
+
+
+// var context = { "title" : "", "author" : "", "genre": "", "checked-in": "", "new-arrival": ""};
+
+// //Compile the template data into a function
+// var templateScript = Handlebars.compile(template);
+
+// var html = templateScript(context);
+// //html = 'My name is Ritesh Kumar . I am a developer.'
+
+// $(document.body).append(html);
+
 document
   .querySelector(".search-form")
   .addEventListener("submit", searchBookTitle);
